b76482168df883ea7251d459223f4c7e26c635ab
Tiago Martins <tiago_gobackto80s@hotmail.com>
2023-09-06 00:22:11 +0100

    Fix serialization. Fix comparison on AStar algorithm.

--- Assets/Scripts/2RGuide/AStar.cs
+++ Assets/Scripts/2RGuide/AStar.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 namespace Assets.Scripts._2RGuide
 {
@@ -15,7 +16,7 @@ namespace Assets.Scripts._2RGuide
     }
 
     [Serializable]
-    public class NodeConnection
+    public struct NodeConnection
     {
         public Node node;
         public ConnectionType connectionType;
@@ -40,7 +41,7 @@ namespace Assets.Scripts._2RGuide
     {
         [SerializeField]
         private Vector2 _position;
-        [SerializeReference]
+        [SerializeField]
         private List<NodeConnection> _connections;
 
         public Vector2 Position 
@@ -64,10 +65,29 @@ namespace Assets.Scripts._2RGuide
         {
             if(obj is Node other)
             {
-                return other.Position == Position;
+                return other.Position.Approximately(Position);
             }
             return false;
         }
+
+        //public static bool operator ==(Node node1, Node node2)
+        //{
+        //    if(node1 == null && node2 == null)
+        //    {
+        //        return true;
+        //    }
+        //    if (node1 == null || node2 == null)
+        //    {
+        //        return false;
+        //    }
+
+        //    return node1.Position.Approximately(node2.Position);
+        //}
+
+        //public static bool operator !=(Node node1, Node node2)
+        //{
+        //    return !(node1 == node2);
+        //}
     }
 
     public class AStar
@@ -92,7 +112,7 @@ namespace Assets.Scripts._2RGuide
             while(queue.Count > 0)
             {
                 var current = queue.Dequeue();
-                if (current == goal)
+                if (current.Equals(goal))
                 {
                     return ReconstructPath(cameFrom, current, goal);
                 }
