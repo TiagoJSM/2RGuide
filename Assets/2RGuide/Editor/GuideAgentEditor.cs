e1e06ec38b755e4d341b6c73c701e760e0e447aa
Tiago Martins <tiago_gobackto80s@hotmail.com>
2023-09-25 18:53:23 +0100

    Add debug info to agent component.

---
+++ Assets/2RGuide/Editor/GuideAgentEditor.cs
@@ -0,0 +1,32 @@
+﻿using _2RGuide;
+using System.Collections;
+using UnityEditor;
+using UnityEngine;
+
+namespace Assets._2RGuide.Editor
+{
+    [CustomEditor(typeof(GuideAgent))]
+    public class GuideAgentEditor : UnityEditor.Editor
+    {
+        private GUIStyle DebugLabelTitleStyle
+        {
+            get
+            {
+                var style = new GUIStyle(EditorStyles.largeLabel);
+                style.fontStyle = FontStyle.Bold;
+                return style;
+            }
+        }
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+
+            var agent = (GuideAgent)target;
+            
+            GUILayout.Label("");
+            GUILayout.Label("Debug Info:", DebugLabelTitleStyle);
+            GUILayout.Label($"State: {agent.Status}");
+            GUILayout.Label($"Current Target Position: {agent.CurrentTargetPosition}");
+        }
+    }
+}
